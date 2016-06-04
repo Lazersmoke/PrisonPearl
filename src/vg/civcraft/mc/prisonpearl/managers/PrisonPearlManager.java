a1bbb02de7b46c005705248cdae542fab8c91330
Rourke750 <Rourke7501@gmail.com>
2016-06-04 19:56:05 -0400

    minor change to pearl logging

--- src/vg/civcraft/mc/prisonpearl/managers/PrisonPearlManager.java
+++ src/vg/civcraft/mc/prisonpearl/managers/PrisonPearlManager.java
@@ -257,7 +257,7 @@ public class PrisonPearlManager {
 		// Log the free'ing PrisonPearl event with coordinates.
 		Player imprisoner = pp.getHolderPlayer();
 		String playerLoc = player != null ? serializeLocation(player.getLocation()) : "[???]";
-		String message = String.format("Something weird has happened with the pearl " + pp.getImprisonedName());
+		String message = String.format("Something weird has happened with the pearl " + pp.getImprisonedName() + ", were they maybe freed by pearl feeding?");
 		
 		if (imprisoner != null) {
 			String imprisonerLoc = serializeLocation(imprisoner.getLocation());
